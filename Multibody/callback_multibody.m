f5721059ad77b57fe6281bbdc3ba5f8b2989f0a6
vincenzospirito <92684710+vincenzospirito@users.noreply.github.com>
2021-10-19 15:22:52 +0200

    Divertitevi
    -Commentato
    -Rettificata la "lettura" delle coordinare dal piano

--- Multibody/callback_multibody.m
+++ Multibody/callback_multibody.m
@@ -1,4 +1,6 @@
 %%
+close all
+clc
 figure(1)
 X=out.x.data;
 Y=out.y.data;
@@ -19,13 +21,11 @@ hold on
 %plot(out.x.time,atan(out.x.data./out.y.data))
 plot(out.angX,'b')
 plot(out.angZ,'r')
-%%
-clc
-X = deg2rad(linspace(0,30,5)');
-Time = seconds(1:5);
-TT = array2timetable(X,'RowTimes',Time)
 
 %%
+close all
+clc
+
 %beam
 rm=2.45 ;           %(cm)  biella
 lm=7.2 ;            %(cm)  manovella
@@ -37,9 +37,21 @@ Mot_Osci_Rang=[-80 ,80];    %(deg)   range di oscilazione dei motori
 
 %plane
 rho_plane=880;         %(kg/m^3) densita del piano
-Lx=14.3;               %(cm)  
-Ly=18.4;               %(cm)
+Lx=14.3;               %(cm)  posizione X della cerniera vanovella_piano
+Ly=18.4 ;               %(cm)   posizione Y della cerniera vanovella_piano
+Lx_plane=15;               %(cm)  dimensione X piano (met della lunghezza)
+Ly_plane=20 ;              %(cm)  dimensione Y piano (met della lunghezza)
 
 %ball
 rho_ball=8000;         % (kg/m^3) densita della sfera
 R_ball=2;              % (cm) raggio della sfera
+X_start=0;             % (cm) posizione X di partenza della palla
+Y_start=0;             % (cm) posizione Y di partenza della palla
+Z_start=0;             % (cm) posizione X di partenza della palla (Z=0 palla e piano in contatto)
+
+%cornice
+Lx_cornice=15;         %(cm) dimensione X della cornice (meta)
+Ly_cornice=20;         %(cm) dimensione Y della cornice (meta)
+H_cornice=0.5;         %(cm) spessore cornice
+W_cornice=2;           %(cm) altezza cornice
+
